6281ac7b3360fa13aab93310f5726cb6d0677a99
mohammedalnaqash <34436094+mohammedalnaqash@users.noreply.github.com>
2018-04-16 20:04:34 +0300

    SC

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $User = new User();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>MLFATI</title>
+<title>SC</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
